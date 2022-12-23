cb13a6fe445c38980cd6251feee0542d99208193
JXX-MX <73275655+JXX-MX@users.noreply.github.com>
2022-12-23 18:59:04 +0900

    feat(searchPage): UI 제작 (#39)

--- src/components/blocks/Header/style.ts
+++ src/components/blocks/Header/style.ts
@@ -5,4 +5,11 @@ export const Header = styled.header`
   justify-content: space-between;
   align-items: center;
   padding: 12px 24px;
+
+  p {
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 17px;
+    color: #000000;
+  }
 `;
