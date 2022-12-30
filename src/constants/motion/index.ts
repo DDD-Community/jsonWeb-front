30c5bb10f3f8c138f0ab41632c18bb2b6291eaf4
Pureum <poo1994@hanyang.ac.kr>
2022-12-30 17:11:52 +0900

    Update index.ts

--- src/constants/motion/index.ts
+++ src/constants/motion/index.ts
@@ -1,25 +1,21 @@
-import React, { PropsWithChildren, ReactElement, useId } from 'react';
-import { AnimatePresence } from 'framer-motion';
+import { Variants } from 'framer-motion';
 
-interface LoadingSpinnerProps {
-  isLoading: boolean;
-  loadingComponent: ReactElement;
-}
+export const defaultEasing = [0.6, -0.05, 0.01, 0.99];
 
-export function LoadingHandler({
-  children,
-  isLoading,
-  loadingComponent,
-}: PropsWithChildren<LoadingSpinnerProps>) {
-  const id = useId();
-
-  return (
-    <AnimatePresence exitBeforeEnter>
-      {isLoading ? (
-        <React.Fragment key={id}>{loadingComponent}</React.Fragment>
-      ) : (
-        children
-      )}
-    </AnimatePresence>
-  );
-}
+export const defaultFadeInVariants: Variants = {
+  initial: {
+    opacity: 0,
+    transition: { duration: 0.3, ease: defaultEasing },
+    willChange: 'opacity',
+  },
+  animate: {
+    opacity: 1,
+    transition: { duration: 0.3, ease: defaultEasing },
+    willChange: 'opacity',
+  },
+  exit: {
+    opacity: 0,
+    transition: { duration: 0.3, ease: defaultEasing },
+    willChange: 'opacity',
+  },
+};
