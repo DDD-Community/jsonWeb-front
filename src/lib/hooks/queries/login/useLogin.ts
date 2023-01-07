76940c57bcac01cacaf27dd70326e5eae0100a7d
MJ LEE <96714229+lxxmnmn@users.noreply.github.com>
2023-01-07 15:21:01 +0900

    Update useLogin.ts
    Login test

--- src/lib/hooks/queries/login/useLogin.ts
+++ src/lib/hooks/queries/login/useLogin.ts
@@ -4,7 +4,7 @@ import { LoginResponseType } from '@src/types/login/login';
 export function useLogin(AUTHORIZE_CODE: string) {
   return get<LoginResponseType>(`/users/login?code=${AUTHORIZE_CODE}`)
     .then(({ data }) => {
-      const JWT: string = data.data.accessToken;
+      const JWT: string = data.data?.accessToken;
       if (JWT) localStorage.setItem('EXIT_LOGIN_TOKEN', JWT);
       window.location.replace('/');
 //     })
