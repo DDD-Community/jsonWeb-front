774082e2552e91e2f21fe8908cf3647450be22c6
MJ LEE <96714229+lxxmnmn@users.noreply.github.com>
2023-01-07 16:14:42 +0900

    Update useLogin.ts
    Login test

--- src/lib/hooks/queries/login/useLogin.ts
+++ src/lib/hooks/queries/login/useLogin.ts
@@ -1,14 +1,24 @@
 import { get } from '@src/api/client';
 import { LoginResponseType } from '@src/types/login/login';
 
-export function useLogin(AUTHORIZE_CODE: string) {
-  return get<LoginResponseType>(`/users/login?code=${AUTHORIZE_CODE}`)
-    .then(({ data }) => {
-      const JWT: string = data.data?.accessToken;
-      if (JWT) localStorage.setItem('EXIT_LOGIN_TOKEN', JWT);
-      window.location.replace('/');
+// export function useLogin(AUTHORIZE_CODE: string) {
+//   return get<LoginResponseType>(`/users/login?code=${AUTHORIZE_CODE}`)
+//     .then(({ data }) => {
+//       const JWT: string = data.data?.accessToken;
+//       if (JWT) localStorage.setItem('EXIT_LOGIN_TOKEN', JWT);
+//       window.location.replace('/');
 //     })
 //     .catch(() => {
 //       window.location.replace('/login');
-    });
-}
+//     });
+// }
+
+export const fetchAuth = (AUTHORIZE_CODE: string): Promise<LoginResponseType> =>
+  get(`/users/login?code=${AUTHORIZE_CODE}`);
+
+export function useLogin(AUTHORIZE_CODE: string) {
+  fetchAuth(AUTHORIZE_CODE).then((response: LoginResponseType) => {
+    const JWT = response.data.data.accessToken;
+    if (JWT) localStorage.setItem('EXIT_LOGIN_TOKEN', JWT);
+    window.location.replace('/');
+  });
