c3e3f63b0d76ca9e75c04c39bf6bc9193a38e8d1
choipureum <pooreumsunny@gmail.com>
2023-01-29 22:05:27 +0900

    .

--- src/lib/hooks/queries/review/useReviewLikeMutation.ts
+++ src/lib/hooks/queries/review/useReviewLikeMutation.ts
@@ -1,14 +1,19 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { put } from '@src/api/client';
+import { useToast } from '@src/store/Toast';
 
 export const putReviewLike = async (reviewId: number) =>
   put(`/reviews/${reviewId}/like`);
 
 export function useReviewLikeMutation({ reviewId }: { reviewId: number }) {
   const queryClient = useQueryClient();
+  const { fireToast } = useToast();
   return useMutation(() => putReviewLike(reviewId), {
-    onSuccess() {
+    onSuccess: () => {
       queryClient.invalidateQueries(['themesReviewListById']);
+    },
+    onError: async () => {
+      fireToast({ content: '좋아요가 실패했어요.⛔️' });
     },
   });
 }
